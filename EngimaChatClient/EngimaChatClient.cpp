71647fb05088f7056f74a26a7b058470a7c5a481
Karol Kopciuch <Karolkopciuch05@gmail.com>
2025-07-14 02:45:26 -0400

    Initial commit

--- EngimaChatClient/EngimaChatClient.cpp
+++ EngimaChatClient/EngimaChatClient.cpp
@@ -2,8 +2,70 @@
 //
 
 #include <iostream>
+#include <winsock2.h>
+#include <WS2tcpip.h>
+#include <tchar.h>
 
-int main()
+
+
+#define PORT 8080
+
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+	GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
+	WORD attributes = consoleInfo.wAttributes;
+	WORD bgColor = (attributes & 0x00F0);
+	WORD ERROR_COLOR = FOREGROUND_RED | bgColor;
+	WORD SUCCESS_COLOR = FOREGROUND_GREEN | bgColor;
+
+	SOCKET clientSocket;
+	int port = PORT;
+	WSADATA wsaData;
+	int wsaerr;
+	WORD wVersionRequested = MAKEWORD(2, 2);
+	wsaerr = WSAStartup(wVersionRequested, &wsaData);
+	if (wsaerr != 0) {
+		SetConsoleTextAttribute(hConsole, ERROR_COLOR);
+		std::cout << "WINSOCK NOT FOUND\n";
+		return 0;
+	}
+	else {
+		SetConsoleTextAttribute(hConsole, SUCCESS_COLOR);
+		std::cout << "WINSOCK FOUND!\n";
+		std::cout << "STATUS: " << wsaData.szSystemStatus << "\n";
+	}
+
+	clientSocket = INVALID_SOCKET;
+	clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (clientSocket == INVALID_SOCKET) {
+		SetConsoleTextAttribute(hConsole, ERROR_COLOR);
+		std::cout << "ERROR AT SOCKET: " << WSAGetLastError() << "\n";
+		return 0;
+	}
+	else {
+		SetConsoleTextAttribute(hConsole, SUCCESS_COLOR);
+		std::cout << "SOCKET IS GOOD!\n";
+	}
+
+	sockaddr_in clientService;
+	clientService.sin_family = AF_INET;
+	InetPton(AF_INET, _T("127.0.0.1"), &clientService.sin_addr.s_addr);
+	clientService.sin_port = htons(port);
+	if (connect(clientSocket, (SOCKADDR*)&clientService, sizeof(clientService)) == SOCKET_ERROR) {
+		SetConsoleTextAttribute(hConsole, ERROR_COLOR);
+		std::cout << "CONNECTION FAILED\n";
+		WSACleanup();
+		return 0;
+	}
+	else {
+		SetConsoleTextAttribute(hConsole, SUCCESS_COLOR);
+		std::cout << "CONNECTION SUCCESS\n";
+		std::cout << "CLIENT CAN SEND AND RECV DATA\n";
+	}
+	system("pause");
+	WSACleanup();
+	return 0;
+
 }
