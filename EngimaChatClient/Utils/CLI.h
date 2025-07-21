e8face3b300c07ac118d22050514ffa1b98caf98
Karol Kopciuch <Karolkopciuch05@gmail.com>
2025-07-20 23:58:03 -0400

    added header file for development of the CLI

---
+++ EngimaChatClient/Utils/CLI.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef CLI_H
+#define CLI_H
+
+
+
+class CLI {
+
+public:
+
+	void readLine();
+	void executeCommand(char in[]);
+
+private:
+	char inLine[];
+
+};
+
+
+
+
+
+
+
+
+
+
+#endif
